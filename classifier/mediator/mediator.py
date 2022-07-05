86f134ce0d2c6e56c161349343d4ca3c69d042f8
Me] <glenbreaks106@gmail.com>
2022-07-05 11:41:10 +0200

    additional skeletons

--- classifier/mediator/mediator.py
+++ classifier/mediator/mediator.py
@@ -1,3 +1,23 @@
+from sklearn.utils.validation import check_X_y
+from sklearn.utils.validation import check_array
+
+
 class Mediator:
     def __init__(self):
+        pass
+
+    def check_train_data(self, X, y):
+        """Check input data for training
+        'check_X_y' in scikitlearn doc:
+
+        """
+        pass
+
+    def check_test_data(self):
+        pass
+
+    def fit_components(self):
+        pass
+
+    def predict_classes(self):
         pass
