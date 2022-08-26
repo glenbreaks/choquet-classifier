bbfe08a6813c7c7ddac22d3d7beadd68311ba832
glenbreaks <glenbreaks106@gmail.com>
2022-08-26 17:25:01 +0200

    implement constraints in parameter estimation

--- classifier/mediator/components/parameter_estimation.py
+++ classifier/mediator/components/parameter_estimation.py
@@ -1,13 +1,14 @@
 import numpy as np
 from scipy import optimize as opt
+from math import comb
 
 from . import moebius_transformation
 from .choquet_integral import ChoquetIntegral
 from . import helper as h
 
 
-    # TODO: function for setting bounds: (eta), gamma, beta
-    # TODO: function for setting linear constraints: moebius normalization, moebius monotonicity in relation to additivity
+# TODO: implement monotonicity condition of moebius implementation
+# TODO: mapping of moebius coefficient to subset
 class ParameterEstimation:
 
     def __init__(self, X, y):
@@ -15,11 +16,38 @@ class ParameterEstimation:
         self.y = y
         self.number_of_features = np.shape(self.X)[1]
 
-    def _log_likelihood_function(self,):
+    def _log_likelihood_function(self, dataset, parameters):
         pass
 
-    def optimize_parameters(self, additivity, eta):
+    def compute_parameters(self, additivity, regularization_parameter=1):
+        bounds, constraints = self._set_constraints(additivity)
+        # pack result from opt.minimize in dict (for moebius coeff)
+        result = opt.minimize()
 
-        #set bounds
-        bounds = opt.Bounds
-        #set linear constraints
+    def _set_constraints(self, additivity):
+        """ Sets up the bounds and linear constraints for the optimization problem.
+            using the Bounds and Constraints classes from scipy.optimize.
+
+        :param additivity: additivity of underlying capacity
+        :return: constraints : list
+                consisting of the Bounds and Constraints instances
+        """
+        number_of_moebius_coefficients = comb(self.number_of_features, additivity)
+
+        # set the bounds - order is: regularization_parameter, gamma, threshold, m(T_1),...,m(T_n) with n being
+        # number_of_moebius_coefficients
+        lower_bound = [1, 1, 0]
+        upper_bound = [np.inf, np.inf, 1]
+
+        linear_constraint_matrix = np.array([0, 0, 0])
+
+        for i in range(number_of_moebius_coefficients):
+            lower_bound.append(0)
+            upper_bound.append(1)
+            linear_constraint_matrix = np.append(linear_constraint_matrix, 1)
+
+        bounds = opt.Bounds(lower_bound, upper_bound)
+        linear_constraint = opt.LinearConstraint(linear_constraint_matrix, [1], [1])
+
+        return bounds, linear_constraint
+
