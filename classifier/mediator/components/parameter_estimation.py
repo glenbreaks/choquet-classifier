8f6f647dacdd683811e742ce6e1f9d3abb3d484f
glenbreaks <glenbreaks106@gmail.com>
2022-09-12 12:16:41 +0200

    added new linear constraint matrix

--- classifier/mediator/components/parameter_estimation.py
+++ classifier/mediator/components/parameter_estimation.py
@@ -8,9 +8,10 @@ from . import helper as h
 # TODO: if additivity=None or 1 linear constraints are not to be created
 class ParameterEstimation:
 
-    def __init__(self, X, y):
+    def __init__(self, X, y, additivity):
         self.X = X
         self.y = y
+        self.additivity = additivity
 
         self.number_of_features = np.shape(self.X)[1]
 
@@ -20,7 +21,7 @@ class ParameterEstimation:
     def _log_likelihood_function(self, dataset, parameters):
         pass
 
-    def compute_parameters(self, additivity, regularization_parameter=1):
+    def compute_parameters(self):
         """
 
         :param additivity:
@@ -36,12 +37,12 @@ class ParameterEstimation:
         ----------
         function has form of: function = {g: x, b: y, 1: m({1}), 2: m({2}),..., }
         """
-        bounds, constraints = self._set_constraints(additivity)
+        bounds, constraints = self._set_constraints()
         # pack result from opt.minimize in dict (for moebius coefficients)
         # result = opt.minimize()
         pass
 
-    def _set_constraints(self, additivity):
+    def _set_constraints(self):
         """ Sets up the bounds and linear constraints for the optimization problem.
             using the Bounds and Constraints classes from scipy.optimize.
 
@@ -50,7 +51,7 @@ class ParameterEstimation:
                 consisting of the Bounds and Constraints instances
         """
 
-        number_of_moebius_coefficients = self._get_number_of_moebius_coefficients(additivity)
+        number_of_moebius_coefficients = self._get_number_of_moebius_coefficients()
 
         # set the bounds - order is: gamma, threshold, m(T_1),...,m(T_n) with n being
         lower_bound = [1, 0]
@@ -71,9 +72,9 @@ class ParameterEstimation:
 
         return bounds
 
-    def get_subset_matrix(self, number_of_moebius_coefficients, additivity):
+    def get_subset_matrix(self, number_of_moebius_coefficients):
         matrix = []
-        for j in h.get_subset_dictionary_list(list(range(1, self.number_of_features + 1)), additivity):
+        for j in h.get_subset_dictionary_list(list(range(1, self.number_of_features + 1)), self.additivity):
             subsets = j
             arr = np.zeros(number_of_moebius_coefficients)
             for i in range(arr.size):
@@ -89,9 +90,9 @@ class ParameterEstimation:
         subset_matrix = np.array(matrix)
         return subset_matrix
 
-    def get_monotonicity_matrix(self, additivity):
-        number_of_moebius_coefficients = self._get_number_of_moebius_coefficients(additivity)
-        set_list = h.get_subset_dictionary_list(list(range(1, self.number_of_features + 1)), additivity)
+    def get_monotonicity_matrix(self):
+        number_of_moebius_coefficients = self._get_number_of_moebius_coefficients()
+        set_list = h.get_subset_dictionary_list(list(range(1, self.number_of_features + 1)), self.additivity)
 
         matrix = []
         for j in set_list:
@@ -106,21 +107,19 @@ class ParameterEstimation:
 
         return monotonicity_matrix
 
+    def get_linear_constraint_matrix(self):
+        monotonicity_matrix = self.get_monotonicity_matrix()
+        boundary_constraint = np.ones(self._get_number_of_moebius_coefficients())
 
-    def _get_linear_constraint_matrix(self, number_of_moebius_coefficients, additivity):
-        number_of_moebius_coefficients = self._get_number_of_moebius_coefficients(additivity)
-
-        linear_constraint_matrix = self._get_linear_constraint_matrix(number_of_moebius_coefficients, additivity)
-
-        linear_constraint_matrix = np.delete(linear_constraint_matrix, 0, 0)
-        linear_constraint_matrix = np.delete(linear_constraint_matrix, np.s_[:2], 1)
-
+        linear_constraint_matrix = np.concatenate(([boundary_constraint], monotonicity_matrix), axis=0)
+        linear_constraint_matrix = np.insert(linear_constraint_matrix, 0, values=0, axis=1)
+        linear_constraint_matrix = np.insert(linear_constraint_matrix, 0, values=0, axis=1)
         return linear_constraint_matrix
 
-    def _get_number_of_moebius_coefficients(self, additivity):
+    def _get_number_of_moebius_coefficients(self):
         number_of_moebius_coefficients = 0
 
-        for i in range(1, additivity + 1):
+        for i in range(1, self.additivity + 1):
             number_of_moebius_coefficients += comb(self.number_of_features, i)
 
         return number_of_moebius_coefficients
--- tests/mediator/components/test_parameter_estimation.py
+++ tests/mediator/components/test_parameter_estimation.py
@@ -16,16 +16,25 @@ class TestParameterEstimation(unittest.TestCase):
         X = [[1, 2, 3, 5], [2, 3, 4, 1], [3, 4, 5, 2], [4, 5, 6, 6]]
         y = [0, 1]
 
-        p = pest.ParameterEstimation(X, y)
+        p = pest.ParameterEstimation(X, y, 2)
 
-        print(p.get_subset_matrix(10, 2))
+        print(p.get_subset_matrix(10))
 
     def test_monotonicity_matrix(self):
         X = [[1, 2, 3, 5], [2, 3, 4, 1], [3, 4, 5, 2], [4, 5, 6, 6]]
         y = [0, 1]
 
-        p = pest.ParameterEstimation(X, y)
+        p = pest.ParameterEstimation(X, y, 3)
 
-        print(p.get_monotonicity_matrix( 3))
+        print(p.get_monotonicity_matrix())
+
+    def test_linear_constraint_matrix(self):
+        X = [[1, 2, 3, 5], [2, 3, 4, 1], [3, 4, 5, 2], [4, 5, 6, 6]]
+        y = [0, 1]
+
+        p = pest.ParameterEstimation(X, y, 3)
+
+        print(p.get_linear_constraint_matrix())
+
 if __name__ == '__main__':
     unittest.main()
