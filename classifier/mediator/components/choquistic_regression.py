2f2e27a6aad24616ab58206e94f5d98f6dbf8f7b
dan <glenbreaks106@gmail.com>
2023-11-18 16:58:09 +0100

    refactored choquistic_regression.py

--- classifier/mediator/components/choquistic_regression.py
+++ classifier/mediator/components/choquistic_regression.py
@@ -1,5 +1,6 @@
-import numpy as np
 
+import numpy as np
+from typing import Dict, List
 from .choquet_integral import ChoquetIntegral
 
 
@@ -9,39 +10,53 @@ class ChoquisticRegression:
     This class implements the Choquistic Regression calculation
     and stores all estimated parameters
 
-    Parameters
-    -------
+    Attributes
+    ----------
     additivity : int
+        Additivity of fuzzy measure.
+
+    scaling : float
+        Scaling parameter for the regression.
+
+    threshold : float
+        Threshold parameter for the regression.
+
+    moebius_coefficients : List[float]
+        List of Moebius coefficients.
+    """
+
+    def __init__(self, additivity: int, parameters: Dict[str, float]) -> None:
+        """
+        Parameters
+        ----------
+        additivity : int
             Additivity of fuzzy measure.
 
-    parameters : dict
+        parameters : Dict[str, float]
             Dictionary, describing all fitted parameters (scaling, threshold,
             moebius coefficients) in the following manner:
             {'gamma': scaling, 'beta': threshold, frozenset({1}): m({1}),...}
-    """
-
-    def __init__(self, additivity, parameters):
+        """
         self.additivity = additivity
         self.scaling = parameters['gamma']
         self.threshold = parameters['beta']
         self.moebius_coefficients = list(parameters.values())[2:]
 
-    def compute_regression_value(self, instance):
+    def compute_regression_value(self, instance: np.ndarray) -> float:
         """Computes the regression value for given instance
          with fitted parameters.
 
         Parameters
-        -------
-        instance:  array-like of shape (1, n_features)
+        ----------
+        instance: np.ndarray
             Instance, where n_features is the number of features.
 
         Returns
         -------
-        regression value: float
+        regression_value : float
             Probability of positive class, computed by the choquistic
             regression model.
         """
-
         choquet_integral = ChoquetIntegral(self.additivity, self.moebius_coefficients)
 
         gamma = self.scaling
